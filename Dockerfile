8f89617f66de5ff1cec6af1e25790a8abe96ae15
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2022-09-13 18:52:14 +0000

    Basic database writing. Now for the fun stuff!

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 from php:8.0-apache as base
 
+RUN docker-php-ext-install mysqli pdo pdo_mysql
 copy ./src /var/www/html
-
--- src/reciever.php
+++ src/reciever.php
@@ -1,5 +1,19 @@
 <?php
-    phpinfo();
-    echo "This is a test";
-    echo getenv('SQL_USER');
+    $server=getenv('SQL_SERVER');
+    $user=getenv('SQL_USER');
+    $password=getenv('SQL_PASSWORD');
+    $database=getenv('SQL_DATABASE');
+    echo $server . $user . $password . $database;
+    try{
+      $conn = new PDO("mysql:host=$server;dbname=$database", $user, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $stmt = $conn->prepare("INSERT INTO test (testval)
+          VALUES (:data)");
+      $stmt->bindParam(':data', $variablehere);
+      $variablehere = 123;
+      $stmt->execute();
+    } catch (PDOException $e) {
+      echo  "SQL error:" . $e->getMessage();
+    }
+    //phpinfo();
 ?>
